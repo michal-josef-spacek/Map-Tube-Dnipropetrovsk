dd8a567c863334ecace602995dcb8812fb25c445
Michal Špaček <tupinek@gmail.com>
2014-12-16 23:56:50 +0100

    First version.

---
+++ examples/ex2.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+# Pragmas.
+use strict;
+use utf8;
+use warnings;
+
+# Modules.
+use Map::Tube::Dnipropetrovsk;
+
+# Object.
+my $obj = Map::Tube::Dnipropetrovsk->new;
+
+# Get XML file.
+my $xml_file = $obj->xml;
+
+# Print out XML file.
+print "XML file: $xml_file\n";
+
+# Output like:
+# XML file: .*/dnipropetrovsk-map.xml
