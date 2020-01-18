62699726d5409af5840e491e23c2ff9117fd5967
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-01-18 11:41:28 +0100

    Update author github username

--- Dnipropetrovsk.pm
+++ Dnipropetrovsk.pm
@@ -189,8 +189,8 @@ For more information about Dnipropetrovsk Map, click L<here|https://en.wikipedia
 
 =begin html
 
-<a href="https://raw.githubusercontent.com/tupinek/Map-Tube-Dnipropetrovsk/master/images/ex3.png">
-  <img src="https://raw.githubusercontent.com/tupinek/Map-Tube-Dnipropetrovsk/master/images/ex3.png" alt="Pražské metro" width="300px" height="300px" />
+<a href="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-Dnipropetrovsk/master/images/ex3.png">
+  <img src="https://raw.githubusercontent.com/michal-josef-spacek/Map-Tube-Dnipropetrovsk/master/images/ex3.png" alt="Pražské metro" width="300px" height="300px" />
 </a>
 
 =end html
@@ -284,7 +284,7 @@ Install the Map::Tube concrete metro modules.
 
 =head1 REPOSITORY
 
-L<https://github.com/tupinek/Map-Tube-Dnipropetrovsk>
+L<https://github.com/michal-josef-spacek/Map-Tube-Dnipropetrovsk>
 
 =head1 AUTHOR
 
